bde004251b480f31fdaeaf16abcfb42f0dbed901
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-09 19:11:56 +0530

    Create assignment2.py

---
+++ Week 2/assignment2.py
@@ -0,0 +1,5 @@
+'''
+Write a program that takes an amount in Indian Rupees as input. You need to find its equivalent in Euro and display it. Assume 1 Rupee equals 80 Euros.  
+'''
+x = int(input())
+print(x*80, end='')
