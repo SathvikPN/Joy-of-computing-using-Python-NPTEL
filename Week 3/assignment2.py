a5809aebca3f6f34ae6481fe206a682f33a53da5
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-10 17:46:39 +0530

    Update assignment2.py

--- Week 3/assignment2.py
+++ Week 3/assignment2.py
@@ -2,11 +2,8 @@ A = []
 for i in range(1,51):
   A.append(i)
 
-a,b = input().split()
-a= int(a)
-b= int(b)
-p = A[a:b].copy()
-l = len(p)
-for i in range(l-1):
-  print(p[i])
-print(p[l-1],end='')
+a,b = map(int, input().split())
+
+for i in range(a,b-1): 
+  print(A[i])
+print(A[b-1],end='') 
