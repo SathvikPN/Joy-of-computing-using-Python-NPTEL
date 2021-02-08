eb910eeeb0039cc2089c40d5282ff6f2882b8165
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2021-02-08 17:32:23 +0530

    Create 3.string-formatting.py

---
+++ Week 12/3.string-formatting.py
@@ -0,0 +1,19 @@
+print([x for x in input().split('@')][1].split('.')[0],end='')
+
+'''
+Assuming that we have some email addresses in the "username@companyname.com" format, please write program to print the company name of a given email address. Both user names and company names are composed of letters only.
+
+Input Format:
+The first line of the input contains an email address.
+
+Output Format:
+Print the company name in single line.
+
+Example;
+
+Input:
+john@google.com
+
+Output:
+google
+'''
